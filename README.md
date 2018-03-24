0d219e21c31a99bc87c777e98f3585c46379cc4e
rongts <rongts@broada.com>
2018-03-24 23:36:32 +0800

    init project

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # 东钱湖单车驿站
-东钱湖单车驿站
+
+# [脑图](http://naotu.baidu.com/file/44bb37933cdc7b2367d300e2b32251ab)
+
+# [API](http://rap.taobao.org/)
+
+# 目录
+
+```
+├──client 客户端
+│     └──mobile-admin 移动端后台
+│     └──pc-admin pc端后台
+│     └──wx-mini-apps 小程序
+├──server 服务端
+```
---
+++ client/mobile-admin/src/providers/global/global.ts
@@ -0,0 +1,13 @@
+import { NavController } from 'ionic-angular';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the GlobalProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class GlobalProvider {
+  tabsNav: NavController
+}
