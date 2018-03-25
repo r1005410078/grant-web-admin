3b7859881d62b3117d40aeb1554c44c810479362
rongts <rongts@broada.com>
2018-03-25 23:34:18 +0800

    niubile

--- client/pc-admin/src/app/app.component.ts
+++ client/pc-admin/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  isCollapsed = false;
+
+  /** custom trigger can be TemplateRef **/
+  changeTrigger(): void {
+
+  }
 }
